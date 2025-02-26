d7ae5a2321010824457cc761233667966a6f6ebc
yagitgadhok <gadhokyagit@gmail.com>
2025-02-26 19:39:51 +0530

    get api made

--- backend/index.js
+++ backend/index.js
@@ -43,12 +43,30 @@ const Url = mongoose.model("Url", urlSchema);
 app.post("/api/short", async (req, res) => {
   try {
     const { originalUrl } = req.body;
+    if (!originalUrl) return res.status(404).json({ error: "Invalid URL" });
     const shortUrl = nanoid(8);
     const url = new Url({ originalUrl, shortUrl });
     await url.save();
     return res
       .status(200)
       .json({ message: "URL Generated Successfully", url: url });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Error Generating URL" });
+  }
+});
+
+app.get("/:shortUrl", async (req, res) => {
+  try {
+    const { shortUrl } = req.params;
+    const url = await Url.findOne({ shortUrl });
+    if (url) {
+      url.clicks++;
+      await url.save();
+      return res.redirect(url.originalUrl);
+    } else {
+      return res.status(404).jsons({ error: "URL not found" });
+    }
   } catch (err) {
     console.log(err);
     return res.status(500).json({ message: "Error Generating URL" });
