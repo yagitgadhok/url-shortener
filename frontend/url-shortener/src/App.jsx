ac1ae5d26f9b28b1fab551608ecaf06253b0a3d9
yagitgadhok <gadhokyagit@gmail.com>
2025-02-26 21:42:33 +0530

    frontend completed

--- frontend/url-shortener/src/App.jsx
+++ frontend/url-shortener/src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import axios from "axios";
 import "./App.css";
 
 function App() {
   const [originalUrl, setOriginalUrl] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
 
   const handleSubmit = () => {
-    console.log("url:", originalUrl);
+    axios
+      .post("http://localhost:3000/api/short", { originalUrl })
+      .then((res) => {
+        setShortUrl(res.data.url.shortUrl);
+        console.log("data:", res);
+      })
+      .catch((err) => {
+        console.log("error:", err);
+      });
   };
 
   return (
@@ -30,6 +38,19 @@ function App() {
       >
         Shorten URL
       </button>
+      {shortUrl && (
+        <div className="mt-6 text-center">
+          <p className="text-lg bold font-medium">Shortened URL:</p>
+          <a
+            href={`http://localhost:3000/${shortUrl}`}
+            className="text-blue-400 underline mt-2"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Short Url
+          </a>
+        </div>
+      )}
     </div>
   );
 }
